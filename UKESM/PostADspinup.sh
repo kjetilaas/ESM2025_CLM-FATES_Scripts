619fe49f5a58b706afd79a0df5bee6ed38f47f98
Kjetil Aas <kjetisaa@login-2.betzy.sigma2.no>
2023-05-03 14:58:24 +0200

    Add/update UKESM scripts

--- UKESM/PostADspinup.sh
+++ UKESM/PostADspinup.sh
@@ -5,9 +5,8 @@ USER="kjetisaa"
 project='nn9188k' #nn8057k: EMERALD, nn2806k: METOS, nn9188k: CICERO
 machine='betzy'
 numCPUs=1024 #Specify number of cpus. 0: use default
-forcenewcase=0 #what to do if case exists. 1: remove
+forcenewcase=1 #what to do if case exists. 1: remove
 model=UKESM1-0-LL #IPSL-CM6A-LR, MPI-ESM1-2-HR, UKESM1-0-LL
-
 
 cimedir="/cluster/work/users/kjetisaa/ESM2025/cime/scripts/"
 usermodsdir="/cluster/work/users/kjetisaa/isimip_forc/historical/$model/user_mods/"
@@ -16,17 +15,18 @@ mkdir -p $casedir
 
 #Setup 
 modelconfig="BGC-CROP" 
-resolution="f19_g17" 
+resolution="f19_g17"
 scenario="1850" 
-versiontag="ADspinup"
+versiontag="postADspinup"
 casename="ESM2025_${model}_${modelconfig}_${scenario}_${resolution}_${versiontag}"
 compset="${scenario}_DATM%GSWP3v1_CLM51%${modelconfig}_SICE_SOCN_SROF_SGLC_SWAV_SESP"
+casename_AD="ESM2025_${model}_${modelconfig}_${scenario}_${resolution}_ADspinup"
 
 stop_n="50"
 stop_opt="nyears"
 job_wc_time="24:00:00"
-resub="4"  
-startyear="0001-01-01"
+resub="13"  
+lastrestarttime="0300-01-01-00000"
 
 if [[ $forcenewcase -eq 1 ]]
 echo $casedir$casename
@@ -51,18 +51,24 @@ echo 'updating settings'
 ./xmlchange --id STOP_N --val $stop_n
 ./xmlchange --id STOP_OPTION --val $stop_opt
 ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=$job_wc_time
-./xmlchange CLM_ACCELERATED_SPINUP="on"
+./xmlchange RUN_TYPE=startup
 ./xmlchange RESUBMIT=$resub
 ./xmlchange DATM_YR_ALIGN=1850
 ./xmlchange DATM_YR_START=1850
 ./xmlchange DATM_YR_END=1869
 ./xmlchange CLM_USRDAT_DIR="/cluster/work/users/kjetisaa/isimip_forc/historical/$model"
-./xmlchange RUN_STARTDATE=$startyear
+
 
 echo "flanduse_timeseries='/cluster/work/users/kjetisaa/isimip_forc/Ohter_modified_files/landuse.timeseries_1.9x2.5_hist_78pfts_CMIP6_simyr1850-2015_ESM2025.nc'" >> user_nl_clm
 echo "fsurdat='/cluster/work/users/kjetisaa/isimip_forc/Ohter_modified_files/surfdata_1.9x2.5_hist_78pfts_CMIP6_simyr1850_ESM2025.nc'" >> user_nl_clm
-echo "use_init_interp=.true." >> user_nl_clm
 echo "stream_fldfilename_ndep = '/cluster/work/users/kjetisaa/isimip_forc/Ohter_modified_files/fndep_clm_hist_b.e21.BWHIST.f09_g17.CMIP6-historical-WACCM.ymonavg_1850-1869_monthly_0.9x1.25_ESM2025.nc'" >> user_nl_clm
+echo "hist_empty_htapes = .true." >> user_nl_clm
+echo "hist_fincl1 = 'TOTECOSYSC', 'TOTECOSYSN', 'TOTSOMC', 'TOTSOMN', 'TOTVEGC', 'TOTVEGN', 'TLAI', 'GPP', 'CPOOL', 'NPP', 'TWS'" >> user_nl_clm
+echo "hist_mfilt = 20" >> user_nl_clm
+echo "hist_nhtfrq = -8760" >> user_nl_clm
+
+# Set the finidat file to the last restart file saved in previous step
+echo " finidat = '${casename_AD}.clm2.r.$lastrestarttime.nc' " >> user_nl_clm
 
 if [[ $numCPUs -ne 0 ]]
 then 
@@ -75,7 +81,14 @@ then
     ./xmlchange NTASKS_GLC=$numCPUs
 fi
 echo 'done with xmlchanges'        
-    
-./case.setup
+
+./case.setup    
 ./case.build
+
+# copy restart files and pointers
+cp /cluster/work/users/kjetisaa/archive/$casename_AD/rest/$lastrestarttime/*.r*.nc /cluster/work/users/kjetisaa/noresm/$casename/run/
+cp /cluster/work/users/kjetisaa/archive/$casename_AD/rest/$lastrestarttime/rpointer.* /cluster/work/users/kjetisaa/noresm/$casename/run/
+echo 'List files in run folder (after copy restart):'
+ls -l /cluster/work/users/kjetisaa/noresm/$casename/run/
+
 ./case.submit
--- checkout.sh
+++ checkout.sh
@@ -2,37 +2,40 @@
 
 #Scrip to clone and checkout ctsm for ESM2025 simulations
 
-dosetup=1 
-
 # what is your clone of the ctsm repo called? (or you want it to be called?) 
-ctsmdir="CTSM" 
-checkout_version="ctsm5.1.dev118"
-
-# aka where do you want the code and scripts to live?
-projectfolder="/cluster/home/kjetisaa/ESM2025_CTSM/" 
-
+ctsmdir="ESM2025" 
+checkout_version="stable_sigma2" #"ctsm5.1.dev120"
 
 #Download code and checkout externals
-if [ $dosetup -eq 1 ] 
+cd "/cluster/work/users/kjetisaa"
+
+#Remove old ctsm code  
+rm -rf $ctsmdir
+
+if [[ -d "$ctsmdir" ]] 
 then
-    cd $projectfolder
+    cd $ctsmdir
+    echo "Already have $ctsmdir folder, do a git pull"
+    git pull
+else
+    module purge
+    module load git/2.36.0-GCCcore-11.3.0-nodocs
+    #module load git/2.23.0-GCCcore-8.3.0 Python/3.7.4-GCCcore-8.3.0
 
-    pwd
-    #go to repo, or checkout code
-    if [[ -d "$ctsmdir" ]] 
-    then
-        cd $ctsmdir
-        echo "Already have ctsm folder"
-    else
-        module purge
-        module load git/2.18.0-GCCcore-7.3.0 Python/3.7.4-GCCcore-8.3.0
-
-        echo "Cloning ctsm"
-        #clone CTSM code if you didn't to this already. 
-        git clone https://github.com/escomp/ctsm $ctsmdir
-        cd $ctsmdir
-        git checkout $checkout_version
-        ./manage_externals/checkout_externals
-        module purge
-    fi
+    echo "Cloning ctsm"
+    #clone CTSM code if you didn't to this already. 
+    #git clone https://github.com/escomp/ctsm $ctsmdir
+    #cd $ctsmdir
+    #git checkout $checkout_version
+    #cp ../../CTSM/Externals.cfg .  #Temporary fix for betzy
+    #git clone https://github.com/mvertens/CTSM
+    #cd CTSM
+    #git checkout feature/update_ccs_config
+    
+    git clone https://github.com/MetOs-UiO/CTSM.git $ctsmdir
+    cd $ctsmdir
+    git checkout $checkout_version
+    
+    ./manage_externals/checkout_externals
+    module purge
 fi
